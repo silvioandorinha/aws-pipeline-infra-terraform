8693efc2f81146844b63665190cc0d9cda0774d4
silvioandorinha <silvioandorinha@gmail.com>
2024-06-01 14:49:13 -0300

    feat: configurações de variaveis de ambiente

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+# base
+terraform {
+  backend "s3" {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# resources
+resource "aws_sns_topic" "example" {
+  name              = "example-topic"
+  kms_master_key_id = "alias/kms-example-topic"
+}
