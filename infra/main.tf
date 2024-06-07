ac414646af0c6b45f19c985587e422018ed3324e
silvioandorinha <silvioandorinha@gmail.com>
2024-06-07 11:14:08 -0300

    Correção

--- infra/main.tf
+++ infra/main.tf
@@ -54,11 +54,6 @@ resource "aws_sqs_queue" "minha_fila_sqs" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.minha_fila_sqs_deadletter.arn
-    maxReceiveCount     = 4
-  })
-
 
   tags = {
     Environment = "dev"
